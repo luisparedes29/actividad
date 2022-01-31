7c321956857bfdb6c9f5d4e1b8f35a3a29db3bb1
Luis Paredes <luisparedescrv@gmail.com>
2022-01-30 23:23:22 -0400

    Segundo cambio

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('Hello Word');
+    res.send('Hola mundo desde express');
 });
 
 app.listen(3000,()=>{
